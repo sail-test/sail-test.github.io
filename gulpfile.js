1d7216168e64e97c6367049cb6958ccecc46fc43
John Wehr <johnwehr@gmail.com>
2015-04-15 15:18:09 -0400

    Sane build

--- gulpfile.js
+++ gulpfile.js
@@ -23,14 +23,7 @@ var browserified = function(filename) {
   }).bundle();
 };
 
-var revision_promise = new Promise(function(resolve, reject){
-  return git.revParse({args:'--short HEAD'}, function (error, hash) {
-    if (error) {
-      return reject(error);
-    }
-    resolve(hash);
-  });
-});
+var BUILD_ID = Math.round(new Date().getTime() / 1000, 0);
 
 gulp.task('development-webserver', function() {
   gulp.src('./')
@@ -51,42 +44,35 @@ gulp.task('development-webserver', function() {
 });
 
 gulp.task('js', ['git-remove'], function(cb) {
-  revision_promise.then(function(revision){
-    return browserified('./js/app.jsx')
-      .pipe(source("app-" + revision + ".js"))
-      .pipe(gulp.dest('dist/src'))
-      .pipe(buffer())
-      .pipe(sourcemaps.init())
-      .pipe(uglify())
-      .pipe(sourcemaps.write('./map', {includeContent: false, sourceRoot: '../src'}))
-      .pipe(gulp.dest('dist'))
-      .on('end', cb);
-  })
+  browserified('./js/app.jsx')
+    .pipe(source("app-" + BUILD_ID + ".js"))
+    .pipe(gulp.dest('dist/src'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./map', {includeContent: false, sourceRoot: '../src'}))
+    .pipe(gulp.dest('dist'))
+    .on('end', cb);
 });
 
 gulp.task('less', ['git-remove'], function(cb) {
-  revision_promise.then(function(revision){
-    return gulp.src('./less/app.less')
-      .pipe(less())
-      .pipe(minify_css())
-      .pipe(rename("app-" + revision + ".css"))
-      .pipe(gulp.dest('dist'))
-      .on('end', cb);
-  })
+  gulp.src('./less/app.less')
+    .pipe(less())
+    .pipe(minify_css())
+    .pipe(rename("app-" + BUILD_ID + ".css"))
+    .pipe(gulp.dest('dist'))
+    .on('end', cb);
 });
 
 gulp.task('templates', function(cb){  
-  revision_promise.then(function(revision){
-    return gulp.src(['./index.html.tmpl'])
-      .pipe(rename("index.html"))
-      .pipe(replace('<GIT-REVISISION>', revision))
-      .pipe(gulp.dest('./'))
-      .on('end', cb);
-  })
+  gulp.src(['./index.html.tmpl'])
+    .pipe(rename("index.html"))
+    .pipe(replace('<BUILD-ID>', BUILD_ID))
+    .pipe(gulp.dest('./'))
+    .on('end', cb);
 });
 
 gulp.task('git-add', ['js', 'less', 'templates'], function(){
-  console.log("Adding.");
   return gulp.src(['./dist/*', './dist/*/*']).pipe(git.add({args: '-f'}));
 });
 
